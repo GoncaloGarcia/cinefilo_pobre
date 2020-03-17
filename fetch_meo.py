c36864cd826e4388b2500ddf5903494d409fc86a
Gonçalo Garcia <goncalo.torrao@talkdesk.com>
2020-03-17 22:30:01 +0000

    Add tests

--- fetch_meo.py
+++ fetch_meo.py
@@ -5,72 +5,54 @@ import datetime
 import io
 import boto3
 import os
+import constants
+import logging
+logging.basicConfig(level = logging.INFO)
 
-channels = ["RTP1","RTP2","SIC","TVI","SICN","RTP3","CMTV", "FOXM","AMCHD","AXBHD","EURCHD","FOXHD","FLIFEH","FOXCHD","FOXCOH","AMCHD","AXBHD","EURCHD","FOXHD","FLIFEH","FOXCHD","FOXCOH","AXNHD","DISNYJ","KSICHD","BIGGS","CART","DISNY","KOMBH","EURS2H","EURHD", "TVCTOPH","TVCEDIH","CINEHD","HOLHD","FOXMH","AMCHD","AXBHD","EURCHD","FOXHD", "FLIFEH","FOXCHD","FOXCOH","AXNHD","AXWHD","SYFHD","TVSEHD","NETFLIX","DEST","DISCVHD", "HISTHD","NGHD","NGWHD","RTPM","QHD","MTVPHD","TLC","SICCHD","MYCHD","24KTHD","TRVHD","LUXHD","FTV", "FTVHD","MCHIC","SMAISHD","TRAHD","RTPACR","RTPMA", "RTPA","TCV","BBC E", "NHK","CGTNHD","CGTNDHD","RUSST","1RUSS","AXNM","AXNMHD", "TVGAL","G VIS","CUBAV","A3INTER","A3SERIE","A3CINE","SOMOS","SOLMUSI","CCOCINA","DECASA", "TV5HD","BFM","BFMB","I24F","FR24F","FR2HD","FR3HD","FR5HD","ARTEHD","RAI1", "DWTVA","EURNA","BVN","PHCNE", "TVCEMOH","TVCACTH","RAINEWS","MAX","PBTVHD","SPTV4K", "EURSP","EURS2","SICK","TVC1","TVC2","TVC3","TVC4","CINE","HOLLW", "FOXM","AMC","AXNBL","EURCH","FOX","FLIFE","FOXCR","FOXCOM","AXN","AXNWH", "SYFY","TVSER","DISCV","HIST","ODISS","NGC","NGWIL","Q","SICR","MTV", "CI","SICC","E!"]
 
-
-query_out = """
-copy ( select  
-	title,
-	array_to_string(ratings, '-'),
-	channels,
-	array_to_string(array_agg(time order by time), E'<br>')
-	
-from 
-	(select tv.title,
-		array_agg(distinct ( movie_ratings.rating , titleid )) as ratings,
-		array_to_string(array_agg(distinct tv.channel), E'<br>') as channels,
-		time
-	from tv, movie_ratings 
-	where tv.title = movie_ratings.title 
-	group by tv.title, time
-	order by ratings desc) as prev
-group by title, channels, ratings
-order by ratings desc) to stdout with delimiter '\t'
-"""
-
-def fetch_and_parse_MEO(url, con):
-    con.cursor().execute("TRUNCATE TABLE tv")
-    for channel in channels:
+def fetch_and_parse_MEO(url, con, s3, string_io):
+    truncate_db(con)
+    for channel in constants.channels:
         date_today = datetime.datetime.now()
         date_lastweek = date_today - datetime.timedelta(days=7)
-        print(date_lastweek.strftime("%Y-%m-%dT%H:%M:%S.000Z"))
         req_body = {"service": "channelsguide", "channels": [channel], "dateStart": date_lastweek.strftime("%Y-%m-%dT%H:%M:%S.000Z"), "dateEnd": date_today.strftime("%Y-%m-%dT%H:%M:%S.000Z"), "accountID": ""}
         r = requests.post(url=url, json=req_body)
-        print(channel)
+        logging.info(channel)
         for channel in r.json()["d"]["channels"]:
             channelName = channel["name"]
             for program in channel["programs"]:
                 progName = program["name"]
                 progDate = program["date"] + " " + program["timeIni"]
-                try:
-                    sql = "INSERT INTO tv (title, channel, time) values ('%s', '%s', '%s')" % (
-                        progName.replace("'", "''"), channelName, progDate)
-                    con.cursor().execute(sql)
-                    con.commit()
-                except Exception as e:
-                    print(e)
+                write_to_postgres(con, channelName, progDate, progName)
 
-    f = io.StringIO()
-    con.cursor().copy_expert(query_out, f)
-
-
-
-    s3 = boto3.resource("s3")
-    print(s3)
+    con.cursor().copy_expert(constants.query_out, string_io)
+    
     try:
-        s3.Bucket('www.cinefilopobre.com').put_object(Key="out.csv", Body=f.getvalue()) 
-        print("Upload Successful")
+        s3.Bucket('www.cinefilopobre.com').put_object(Key="out.csv", Body=string_io.getvalue()) 
+        logging.info("Upload Successful")
     except FileNotFoundError:
-        print("The file was not found")
+        logging.info("The file was not found")
     except NoCredentialsError:
-        print("Credentials not available")
+        logging.info("Credentials not available")
 
+def truncate_db(con):
+    con.cursor().execute("TRUNCATE TABLE tv")
+
+def write_to_postgres(con, channelName, progDate, progName):
+    try:
+        sql = "INSERT INTO tv (title, channel, time) values ('%s', '%s', '%s')" % (
+        progName.replace("'", "''"), channelName, progDate)
+        con.cursor().execute(sql)
+        con.commit()
+    except Exception as e:
+        logging.info(e)
 
 def lambda_handler(event, context):
     url = "https://www.meo.pt/_layouts/15/Ptsi.Isites.GridTv/GridTvMng.asmx/getProgramsFromChannels"
+    string_io = io.StringIO()
     con = psycopg2.connect(database="cinefilo", user="cinefilo", password="cinefilo",
                            host="cinefilopobre.ckpkwbhif9f0.us-east-1.rds.amazonaws.com", port="5432")
-    fetch_and_parse_MEO(url, con)
+    s3 = boto3.resource("s3")
+    fetch_and_parse_MEO(url, con, s3, string_io)
     con.close()
 
--- fetch_meo_test.py
+++ fetch_meo_test.py
@@ -1,24 +1,56 @@
 import unittest
 from unittest.mock import Mock
+from unittest.mock import patch
 import fetch_meo
 import responses
 import constants
 import json
+import logging
+logging.basicConfig(level = logging.DEBUG)
 
 class testMEOFetch(unittest.TestCase):
     
     
 
     @responses.activate
-    def test_abc(self):
+    def test_s3_bucket_update(self):
         mock_url = "http://www.meo.pt"
         con = Mock()
         s3 = Mock()
+        string_io = Mock()
+
         responses.add(responses.POST, mock_url,json=json.loads(constants.example_json), status=200)
-        fetch_meo.fetch_and_parse_MEO(mock_url, con, s3)
+        string_io.getvalue.return_value = "test_value"
 
+        fetch_meo.fetch_and_parse_MEO(mock_url, con, s3, string_io)
+        
+        s3.Bucket.assert_called_with('www.cinefilopobre.com')
+        s3.Bucket('www.cinefilopobre.com').put_object.assert_called_with(Key='out.csv', Body="test_value")
 
+    @responses.activate
+    def test_insert_postgres_called_for_each_program_and_channel(self):
+        mock_url = "http://www.meo.pt"
+        con = Mock()
+        s3 = Mock()
+        string_io = Mock()
 
+        responses.add(responses.POST, mock_url,json=json.loads(constants.example_json), status=200)
+
+        fetch_meo.fetch_and_parse_MEO(mock_url, con, s3, string_io)
+        assert con.cursor().execute.call_count == len(constants.channels) + 1 # +1 for the truncate
+
+    def test_db_truncate(self):
+        con = Mock()
+        fetch_meo.truncate_db(con)
+        con.cursor().execute.assert_called_with("TRUNCATE TABLE tv")
+
+    def test_write_to_postgres(self):
+        con = Mock()
+        channelName = "testChannel"
+        progDate = "testDate"
+        progName = "testProgram"
+        fetch_meo.write_to_postgres(con, channelName, progDate, progName)
+        con.cursor().execute.assert_called_with('INSERT INTO tv (title, channel, time) values (\'testProgram\', \'testChannel\', \'testDate\')')
 
 if __name__ == '__main__':
     unittest.main()
